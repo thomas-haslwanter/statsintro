5b364a7912fd289cbb0714ffa31e96661bf4ff21
Thomas Haslwanter <thomas.haslwanter@fh-linz.at>
2013-03-13 12:05:02 +0100

    infos added

--- Code/regSpector.py
+++ Code/regSpector.py
@@ -1,13 +1,22 @@
 '''Linear Regression
 Estimation of a linear regression model using the Spector and Mazzeo (1980) data set.
 
+Documentation:
+    data on 32 students TUCE scores
+    5 columns with rows = students
+
+    1) Grade ..... post grade
+    2) constant .. term
+    3) psi
+    4) tuce ...... tuce (test of understanding of college economics) score
+    5) gpa ....... grade point average
+
 '''
 
 '''
 Author: Bruno Rodrigues
-corr:   ThH
 Date:   March-2013
-Ver:    1.1
+Ver:    1.2 (Thomas Haslwanter)
 '''
 
 # For this we only need to import statsmodels
@@ -18,7 +27,7 @@ import statsmodels.api as sm
 data = sm.datasets.spector.load_pandas()
 
 # We define y as the endogenous variable, and x as the 
-# exogenous variable
+# exogenous variable.
 # Note that if you load your own data, the methods endog 
 # and exog will not be available and you will have to 
 # explicitly define the endogenous and exogenous variables
@@ -28,12 +37,17 @@ y, x = data.endog, data.exog
 reg = sm.OLS(y, x).fit()
 
 # And here we can see the results in a very nice looking table
+print('SUMMARY -------------------------------------------')
 print reg.summary()
 
 # We can only take a look at the parameter values though
+print('PARAMETERS ----------------------------------------')
 print reg.params
 
 # We can also extract the residuals
+print('RESIDUALS -----------------------------------------')
 print reg.resid
 
-raw_input('Done')
+# This line is just to prevent the output from vanishing when you
+# run the program by double-clicking
+raw_input('Done - Hit any key to finish.')
