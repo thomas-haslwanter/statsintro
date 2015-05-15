9eff4b0a01480117846b6682117cef80f6923021
Thomas Haslwanter <thomas.haslwanter@alumni.ethz.ch>
2015-05-15 23:17:02 +0200

    Population and samples added

---
+++ Code3/showSine.py
@@ -0,0 +1,19 @@
+'''Plot a sine-wave
+
+Author: Thomas Haslwanter
+Date:   May-2015
+Ver:    1.0
+
+'''
+
+# Import packages
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Calculate the data
+t = np.arange(0,10,0.1)
+x = np.sin(t)
+
+# Plot the data
+plt.plot(t,x)
+plt.show()
