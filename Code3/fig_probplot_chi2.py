9e7e5718a8ea8ee42a247a24619aaea534dc2719
Thomas Haslwanter <thomas.haslwanter@alumni.ethz.ch>
2015-05-26 15:52:29 +0200

    The first 100 pages! Up to and including chapter 7.

--- Code3/fig_probplot_chi2.py
+++ Code3/fig_probplot_chi2.py
@@ -2,7 +2,7 @@
 Demonstration of the probplot of a non-normal distribution
 '''
 
-# author: Thomas Haslwanter, date: July-2014
+# author: Thomas Haslwanter, date: May-2015
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,7 +17,9 @@ chi2 = stats.chi2(3)
 # Generate the data
 x = np.linspace(0,10, 100)
 y = chi2.pdf(x)
-data = chi2.rvs(100)
+np.random.seed(12345)
+numData = 100
+data = chi2.rvs(numData)
 
 # Arrange subplots
 sns.set_context('paper')
@@ -46,6 +48,18 @@ x0, x1 = axs[1].get_xlim()
 y0, y1 = axs[1].get_ylim()
 axs[1].set_aspect((x1-x0)/(y1-y0))
 #sns.despine()
+
+'''
+# To get an idea how probplot works, calculate the quantiles directly.
+# in "probplot", Filliben's estimate is used, which changes the values slightly
+data.sort()
+mark_y = data[:-1]
+
+nd = stats.norm()
+mark_x = nd.ppf(np.arange(1,len(data))/numData)
+axs[1].plot(mark_x, mark_y, 'rx-')
+'''
+
 
 mystyle.printout_plain('chi2pp.png')
 
