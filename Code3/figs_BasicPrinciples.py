00890b8a5d64b66e83d0edad6fb8a7dbbf146a65
Thomas Haslwanter <thomas.haslwanter@alumni.ethz.ch>
2015-06-12 22:36:09 +0200

    3d plots added

--- Code3/figs_BasicPrinciples.py
+++ Code3/figs_BasicPrinciples.py
@@ -55,7 +55,12 @@ def main():
     # Cumulative probability density
     numbins = 20
     plt.plot(stats.cumfreq(x,numbins)[0])
-    mystyle.printout('CumulativeFrequencyFunction.png', xlabel='Data Values', ylabel='Cumulative Frequency')
+    mystyle.printout('CumulativeFrequencyFunction.png', xlabel='Data Values', ylabel='CumFreq', title='Cumulative Frequncy')
+
+    # KDE-plot
+    sns.kdeplot(x)
+    mystyle.printout('kde.png', xlabel='Data Values', ylabel='Density',
+            title='KDE_plot')
     
     # Boxplot
     # The ox consists of the first, second (middle) and third quartile
@@ -88,7 +93,68 @@ def main():
     #sns.violinplot(df, color = ["#999999", "#DDDDDD"])
     sns.violinplot(df)
     
-    mystyle.printout('violinplot.png')
+    mystyle.printout('violinplot.png', title='Violinplot')
+    
+    # Barplot
+    df = pd.DataFrame(np.random.rand(10, 4), columns=['a', 'b', 'c', 'd'])
+    df.plot(kind='bar', grid=False)
+    mystyle.printout('barplot.png', title='Barplot')
+
+    # Grouped Boxplot
+    sns.set_style('whitegrid')
+    sns.boxplot(df)
+    mystyle.printout('groupedBoxplot.png', title='sns.boxplot')
+
+    # Bivariate Plots
+    df2 = pd.DataFrame(np.random.rand(50, 4), columns=['a', 'b', 'c', 'd'])
+    df2.plot(kind='scatter', x='a', y='b', s=df['c']*300);
+    mystyle.printout('bivariate.png')
+
+    # Pieplot
+    series = pd.Series(3 * np.random.rand(4), index=['a', 'b', 'c', 'd'], name='series')
+    sns.set_palette("husl")
+    series.plot(kind='pie', figsize=(6, 6))
+    mystyle.printout('piePlot.png', title='pie-plot')
+
+def scatterplot():
+    import seaborn as sns
+    sns.set()
+    sns.set_context('poster')
+    
+    df = sns.load_dataset("iris")
+    sns.pairplot(df, hue="species", size=2.5)    
+    mystyle.printout_plain('multiScatterplot.png')
+    
+def show3D():
+    # imports specific to the plots in this example
+    import numpy as np
+    from matplotlib import cm
+    from mpl_toolkits.mplot3d.axes3d import get_test_data
+    
+    # Twice as wide as it is tall.
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    
+    #---- First subplot
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    X = np.arange(-5, 5, 0.1)
+    Y = np.arange(-5, 5, 0.1)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
+            linewidth=0, antialiased=False)
+    ax.set_zlim3d(-1.01, 1.01)
+    
+    fig.colorbar(surf, shrink=0.5, aspect=10)
+    
+    #---- Second subplot
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    X, Y, Z = get_test_data(0.05)
+    ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+
+    mystyle.printout_plain('3dGraph.png')
     
 if __name__ == '__main__':
     main()
+    scatterplot()
+    show3D()
